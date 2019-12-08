3256756a8256b9b9ae21afe1a793caa1cf2349b9
Shanu Dey <sd4shanudey@gmail.com>
2019-12-08 13:04:15 +0530

    Update compile_script.sh

--- compile_script.sh
+++ compile_script.sh
@@ -5,11 +5,11 @@ export ARCH=arm64
 export SUBARCH=arm64 
 
 echo "Setting KBUILD Configuration"
-export KBUILD_BUILD_USER="ShanuDey" 
+export KBUILD_BUILD_USER="Developer" 
 export KBUILD_BUILD_HOST="Equinox" 
 
 echo "setting cross_compiler"
-export CROSS_COMPILE=$(pwd)/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=$(pwd)/GCC-4.9-x64/bin/aarch64-linux-android-
 export KBUILD_COMPILER_STRING=$($(pwd)/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 export CROSS_COMPILE_ARM32=$(pwd)/GCC-4.9-x32/bin/arm-linux-androideabi-
 
