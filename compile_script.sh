fb34bfd5ecd6960694d36c1179175617d0047b40
Shanu Dey <sd4shanudey@gmail.com>
2019-12-08 14:51:04 +0530

    Replace PWD with full path

--- compile_script.sh
+++ compile_script.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "path="
+pwd
+echo "List of files/directories="
+ls
+
 echo "Setting Achitecture"
 export ARCH=arm64 
 export SUBARCH=arm64 
@@ -9,9 +14,9 @@ export KBUILD_BUILD_USER="Developer"
 export KBUILD_BUILD_HOST="Equinox" 
 
 echo "setting cross_compiler"
-export CROSS_COMPILE=$(pwd)/GCC-4.9-x64/bin/aarch64-linux-android-
-export KBUILD_COMPILER_STRING=$($(pwd)/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
-export CROSS_COMPILE_ARM32=$(pwd)/GCC-4.9-x32/bin/arm-linux-androideabi-
+export CROSS_COMPILE=/home/runner/work/HMP/HMP/GCC-4.9-x64/bin/aarch64-linux-android-
+export KBUILD_COMPILER_STRING=$(/home/runner/work/HMP/HMP/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
+export CROSS_COMPILE_ARM32=/home/runner/work/HMP/HMP/GCC-4.9-x32/bin/arm-linux-androideabi-
 
 echo "making out directory"
 mkdir -p out
@@ -25,7 +30,7 @@ make O=out ARCH=arm64 X00T_defconfig
 
 echo "kernel compiling stated"
 make -j$(nproc --all) O=out ARCH=arm64 \
-                        CC="$(pwd)/clang/bin/clang" \
+                        CC="/home/runner/work/HMP/HMP/clang/bin/clang" \
                         CLANG_TRIPLE="aarch64-linux-gnu-" \
                         2>&1 | tee kernel_build_log.txt
                         
